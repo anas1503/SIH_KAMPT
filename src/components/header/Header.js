0f4792fcf552f6292577d893b3c5e59910ff4183
anas1503 <md.anas1503@gmail.com>
2022-03-16 13:15:54 +0530

    build header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,6 +5,10 @@ const Header = () => {
   return (
     <div className="header">
         <p className="logo">KAMPT</p>
+        <div className="login">
+            <p>Login</p>
+            <submit>Sign UP</submit>
+        </div>
     </div>
   )
 }
